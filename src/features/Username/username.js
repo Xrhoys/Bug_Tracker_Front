faa33785c0bb25e342b756e064c0c6e58b63a8b8
Xrhoys <yefeng@hotmail.fr>
2020-10-10 23:51:53 +0200

    usename feature

--- src/features/Username/username.js
+++ src/features/Username/username.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from './../../app/user'
 
 export default function Username(props) {
+  const user = useSelector(selectCurrentUser)
+
+  const name = user ? user.name : 'Login'
   return (
-    <div>
-      <p>{}</p>
+    <div onClick={props.onClick}>
+      <p>{name}</p>
     </div>
   )
+}
+
+Username.propTypes = {
+  onClick: PropTypes.func,
 }
