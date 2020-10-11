c8311419fcc79a981d62c3b2c8a813a38638ed6c
Xrhoys <yefeng@hotmail.fr>
2020-10-11 23:38:30 +0200

    Added icon to username feature

--- src/features/Username/username.js
+++ src/features/Username/username.js
@@ -2,6 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { selectCurrentUser } from './../../app/user'
+import ChevronDown from './../../assets/svg/chevron_down'
+
+const CHEVRON_COLOR = '#707070'
+const CHEVRON_WIDTH = '14px'
+const CHEVRON_HEIGHT = 'auto'
+const CHEVRON_STROKE = '2px'
 
 export default function Username(props) {
   const user = useSelector(selectCurrentUser)
@@ -10,6 +16,12 @@ export default function Username(props) {
   return (
     <div onClick={props.onClick}>
       <p>{name}</p>
+      <ChevronDown
+        fill={CHEVRON_COLOR}
+        width={CHEVRON_WIDTH}
+        height={CHEVRON_HEIGHT}
+        strokeWidth={CHEVRON_STROKE}
+      />
     </div>
   )
 }
