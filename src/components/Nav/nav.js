3dfb805255f5c633c439c429ffc8e7432e83d70a
Xrhoys <yefeng@hotmail.fr>
2020-10-12 21:47:54 +0200

    updated navlink

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -9,6 +9,13 @@ import Gear from './../../assets/svg/gear'
 import Person from './../../assets/svg/person'
 import NavBanner from './../../features/NavBanner/NavBanner'
 
+const LINK_DASHBOARD = 'dashboard'
+const LINK_ISSUES = 'issues'
+const LINK_PROJECTS = 'projects'
+const LINK_PROFILE = 'profile'
+const LINK_MESSAGE = 'message'
+const LINK_SETTINGS = 'settings'
+
 export default function Nav(props) {
   return (
     <div className={styles.container}>
@@ -17,16 +24,38 @@ export default function Nav(props) {
         text="Dashboard"
         icon={Dashboard}
         onClick={() => alert('CLICKED')}
+        routerLink={LINK_DASHBOARD}
       />
-      <NavLink text="Issues" icon={CPU} onClick={() => alert('CLICKED')} />
+      <NavLink
+        text="Issues"
+        icon={CPU}
+        onClick={() => alert('CLICKED')}
+        routerLink={LINK_ISSUES}
+      />
       <NavLink
         text="Projects"
         icon={Collection}
         onClick={() => alert('CLICKED')}
+        routerLink={LINK_PROJECTS}
       />
-      <NavLink text="Profile" icon={Person} onClick={() => alert('CLICKED')} />
-      <NavLink text="Message" icon={Chat} onClick={() => alert('CLICKED')} />
-      <NavLink text="Settings" icon={Gear} onClick={() => alert('CLICKED')} />
+      <NavLink
+        text="Profile"
+        icon={Person}
+        onClick={() => alert('CLICKED')}
+        routerLink={LINK_PROFILE}
+      />
+      <NavLink
+        text="Message"
+        icon={Chat}
+        onClick={() => alert('CLICKED')}
+        routerLink={LINK_MESSAGE}
+      />
+      <NavLink
+        text="Settings"
+        icon={Gear}
+        onClick={() => alert('CLICKED')}
+        routerLink={LINK_SETTINGS}
+      />
     </div>
   )
 }
--- src/features/NavLink/NavLink.js
+++ src/features/NavLink/NavLink.js
@@ -6,46 +6,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './NavLink.module.css'
+import { Link } from 'react-router-dom'
 
 const DEFAULT_ICON_COLOR = '#C7CADD'
 const DEFAULT_ICON_COLOR_HIGHLIGHT = '#5E81F4'
 const DEFAULT_ICON_HEIGHT = '16px'
 
 export default function Counter(props) {
-  const { icon: Icon, text, selected, onClick } = props
+  const { icon: Icon, text, routerLink } = props
 
-  if (selected) {
+  const pathname = window.location.pathname.replace('/', '')
+
+  if (routerLink === pathname) {
     return (
-      <div className={styles.container} onClick={onClick}>
+      <Link to={`/${routerLink}`}>
+        <div className={styles.container}>
+          <Icon
+            fill={DEFAULT_ICON_COLOR_HIGHLIGHT}
+            height={DEFAULT_ICON_HEIGHT}
+            width="auto"
+            strokeWidth={2}
+          />
+          <p className={['no-select', styles.titleSelected].join(' ')}>
+            {text}
+          </p>
+        </div>
+      </Link>
+    )
+  }
+  return (
+    <Link to={`/${routerLink}`}>
+      <div className={styles.container}>
         <Icon
-          fill={DEFAULT_ICON_COLOR_HIGHLIGHT}
+          fill={DEFAULT_ICON_COLOR}
           height={DEFAULT_ICON_HEIGHT}
           width="auto"
           strokeWidth={2}
         />
-        <p className={['no-select', styles.titleSelected].join(' ')}>{text}</p>
+        <p className={['no-select', styles.title].join(' ')}>{text}</p>
       </div>
-    )
-  }
-  return (
-    <div className={styles.container} onClick={onClick}>
-      <Icon
-        fill={DEFAULT_ICON_COLOR}
-        height={DEFAULT_ICON_HEIGHT}
-        width="auto"
-        strokeWidth={2}
-      />
-      <p className={['no-select', styles.title].join(' ')}>{text}</p>
-    </div>
+    </Link>
   )
 }
 
 Counter.propTypes = {
   icon: PropTypes.object.isRequired,
   text: PropTypes.string.isRequired,
-  selected: PropTypes.bool,
-}
-
-Counter.defaultProps = {
-  selected: false,
+  routerLink: PropTypes.string,
 }
