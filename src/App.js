ccda024942d0bafbd9bd090dca1ba623266c1c39
Xrhoys <yefeng@hotmail.fr>
2020-10-13 18:51:20 +0200

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './App.css'
-import { Nav } from './components'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SignIn from './pages/signIn'
+import SignUp from './pages/signUp'
+import Home from './pages/home'
+import Main from './pages/main'
 
 function App() {
   return (
-    <div>
-      <Nav />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/main">
+            <Main />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav, MainPanel } from './../components'
+
+export default function Main(props) {
+  return (
+    <>
+      <Nav />
+      <MainPanel />
+    </>
+  )
+}
