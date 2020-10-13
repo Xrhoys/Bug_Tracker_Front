db15ac9db096faf5ec4104552eea138b78af30b7
Xrhoys <yefeng@hotmail.fr>
2020-10-13 18:52:25 +0200

    updated topbar

--- src/features/PanelTopBar/topbar.js
+++ src/features/PanelTopBar/topbar.js
@@ -3,23 +3,23 @@ import styles from './topbar.module.css'
 import { useSelector } from 'react-redux'
 import { selectCurrentUser } from './../../app/user'
 import Dropdown from './../Dropdown/dropdown'
+import { Dropdown as Drop } from 'react-bootstrap'
+import { selectCurrentContainer } from '../../app/containerController'
 
 export default function Topbar(props) {
-  const pathname = window.location.pathname.replace('/', '').toUpperCase()
   const user = useSelector(selectCurrentUser)
+  const pathname = useSelector(selectCurrentContainer)
 
   const trigger = <p className={styles.username}>{user.name}</p>
 
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
-        <h1 className={styles.title}>{pathname}</h1>
+        <h1 className={styles.title}>{pathname.toUpperCase()}</h1>
         <Dropdown trigger={trigger}>
-          <>
-            <div>
-              <p>Logout</p>
-            </div>
-          </>
+          <Drop.Menu className={styles.dropdownMenu}>
+            <Drop.Item className={styles.dropdownItem}> LOGOUT </Drop.Item>
+          </Drop.Menu>
         </Dropdown>
       </div>
       <div className={styles.separator} />
