56d15017c1f475fa468f1f5ebdd11106b57ecfc3
Xrhoys <yefeng@hotmail.fr>
2020-10-14 23:20:03 +0200

    Updates on sign in page

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './signin.module.css'
+import Bug from './../assets/svg/bug'
+import { Link } from 'react-redux'
+
+const BUG_COLOR = '#5E81F4'
+const BUG_WIDTH = '60px'
+const BUG_HEIGHT = 'auto'
+const SUBMIT = 'Sign In'
 
 export default function SignIn(props) {
-  return <div />
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onChangeUsername = (event) => setUsername(event.target.value)
+  const onChangePassword = (event) => setPassword(event.target.value)
+
+  const onSubmit = (event) => {}
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.windowContainer}>
+        <div className={styles.topPart}>
+          <Bug fill={BUG_COLOR} width={BUG_WIDTH} height={BUG_HEIGHT} />
+          <h1>Bug Tracker</h1>
+        </div>
+        <div>
+          <form className={styles.inputsPart} onSubmit={}>
+            <input
+              type="text"
+              className={styles.text}
+              onChange={onChangeUsername}
+              value={username}
+            />
+            <input
+              type="password"
+              className={styles.text}
+              onChange={onChangePassword}
+              value={password}
+            />
+            <input className={styles.submitButton} value={SUBMIT} />
+          </form>
+          <p className={styles.signUp}>
+            Get involved!
+            <Link>
+              <u>Sign Up</u>
+            </Link>
+          </p>
+          <Link>
+            <p className={styles.forgotPassword}>Forgot Password?</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
